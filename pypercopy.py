d4415dfdd2831564762d336916764f7d177d71c4
fikesoft <sorinchirtoaca34@gmail.com>
2023-03-28 23:27:42 +0200

    #2 Added function that allows to save into a name.txt file

--- pypercopy.py
+++ pypercopy.py
@@ -2,39 +2,38 @@ import pyperclip
 import requests
 import time
 
-start=input("This app will copy the link that you copied in your clipboard \n press ENTER ")
-
-url= pyperclip.paste()
+start = input("This app will copy the link that you copied in your clipboard. Press ENTER to start")
 
 def its_valid(url):
-    #This function will check if the link exist and can be instaled  
-    
-    #Request 
-    answer = requests.head(url)
-    
-    #Making a condition because if the http status code is 200 
-    #the request has been succed and returned data 
-    if answer.status_code == 200:
-        return True
-    
-    else: 
-        return False  
-
-if its_valid(url)==True:
+    try:
+        #Making some request 
+        response = requests.head(url)
+        #This returns  True if the request is 200 
+        return response.status_code == 200
+    except:
+        print("Invalid url")
+        return False 
 
 # Open a file in write mode
-    with open("C:/Users/Sorin Chirtoaca/Desktop/copi/save_photo/links.txt", 'w') as file:
-        while True: 
 
-        
-            # Write clipboard contents to the file
-            file.write(url + '\n')
-            print("Link saved to file!")
-            print("You have 10 seconds to find next link")
-            # Here i will continue   making an timer 
-            #After this i will improve the begin of my program 
+while True: 
+        # Open a file in write mode
+    file=open("C:/Users/Sorin Chirtoaca/Desktop/copi/Saved_photo/links.txt", 'a')
+    # Update the variable
+    url = pyperclip.paste()
+    if its_valid(url)==True:
+        # Write clipboard contents to the file
+        print(url)
+        file.write(url + '\n')
+        print("Link saved to file!")
+        file.close()
+        x=input("Press any button to continue or q to exit")
             
-            
-    # Close the file
-    file.close()
-        
+        if x=="q":
+            break
+        else:
+            pass
+
+    else:
+        print("Invalid link or unable to access link.")
+        break
