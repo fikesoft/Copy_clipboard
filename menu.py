a5cf2c21bdaab268aad6ec2ce7cd020b1b17e1f0
fikesoft <sorinchirtoaca34@gmail.com>
2023-03-29 10:12:51 +0200

    IDK

---
+++ menu.py
@@ -0,0 +1,25 @@
+#In this file  will be a cmd menu that allows
+#To set up the method that you will use the program
+def menu():
+    ret= '''    
+    ---------------------------------------
+    1)Use the automatic checking of clipboard
+    2)Use the controled method of copying  
+    3)Dowland photo with the file with links created 
+    0)Exit 
+    -----------------------------------------
+    '''
+    return ret 
+
+#An bucle that will maintain the menu 
+while True:
+    print(menu())
+    x=input("What method would you use ?")
+    if x=="1":
+        pass
+    if x=="2":
+        pass
+    if x=="3":
+        pass
+    if x=="0":
+        break
